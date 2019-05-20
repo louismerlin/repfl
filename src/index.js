989f200a359a81f62b65be26e59a175c591d04d8
Louis Merlin <louis_merlin1@yahoo.fr>
2019-05-20 14:00:19 +0200

    Make a few style changes

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function Main () {
   return (
     <main class='container'>
 
-      <header>
+      <header class='header'>
         <h1 class='less-margin title'>rEPFL</h1>
         <h3 class='less-margin'>find a free room @ EPFL</h3>
       </header>
@@ -36,7 +36,7 @@ function Main () {
       </p>
 
       {buildings.map(building => (
-        <div className={selectedBuilding !== 'all' && selectedBuilding !== building && 'hidden'}>
+        <div class={selectedBuilding !== 'all' && selectedBuilding !== building && 'hidden'}>
           <h2 class='text-center table-h2'>{building.toUpperCase()}</h2>
           <hr class='table-hr' />
           <table>
@@ -47,8 +47,9 @@ function Main () {
         </div>
       ))}
 
-      <footer>
-        <h5 class='less-margin'>made with {'<3'} by <a href='https://louismerl.in'>Louis Merlin</a></h5>
+      <footer class='footer'>
+        <hr />
+        <h5 class='less-margin'>made with ❤️ by <a href='https://louismerl.in'>Louis Merlin</a></h5>
         <h5 class='less-margin'>using <a href='https://preactjs.com'>preact</a> and <a href='https://concrete.style'>concrete.css</a></h5>
         <h5 class='less-margin'>need more rooms ? <a href='https://github.com/louismerlin/repfl/blob/master/src/rooms.json'>add them</a> or <a href='mailto:louis.merlin@epfl.ch'>email me</a></h5>
         <h5 class='less-margin'>don't hesitate to <a href='https://github.com/louismerlin/repfl'>star and/or contribute</a></h5>
