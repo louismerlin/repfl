c60f5a66b31ef36fa721bd5717acd61426127be7
Louis Merlin <louis_merlin1@yahoo.fr>
2019-08-26 18:05:01 +0200

    Modify service worker to only save non-cors requests

--- src/sw.js
+++ src/sw.js
@@ -1,10 +1,10 @@
 /* eslint-env serviceworker */
+
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('v1').then(cache => {
+    caches.open('v7').then(cache => {
       return cache.addAll([
-        '/',
-        'https://cors.louis.workers.dev'
+        '/'
       ])
     })
   )
@@ -12,9 +12,10 @@ self.addEventListener('install', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(caches.match(event.request).then(response => {
+
     // caches.match() always resolves
     // but in case of success response will have value
-    if (response !== undefined) {
+    if (response !== undefined && response.type === 'basic') {
       return response
     } else {
       return fetch(event.request).then(response => {
@@ -23,7 +24,7 @@ self.addEventListener('fetch', event => {
         // and serve second one
         const responseClone = response.clone()
 
-        caches.open('v1').then(cache => {
+        caches.open('v7').then(cache => {
           cache.put(event.request, responseClone)
         })
         return response
